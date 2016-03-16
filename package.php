073d0521f47b730984fa253704f83ea2ed0efbe8
Kayila <arekuanubis@gmail.com>
2016-03-15 22:36:47 -0400

    Added package.php

---
+++ package.php
@@ -0,0 +1,11 @@
+<?php
+
+$packages[] = array(
+    'name' => 'Pokecube',
+    'title' => 'Pokecube',
+    'version' => 'VERSION',
+    'priority' => 0,
+    'location' => 'modpacks/pokecube.json',
+);
+
+?>
